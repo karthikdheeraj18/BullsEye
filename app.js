3e06b1cb18174e75a46b14ff54e9848f151047b0
Karthik Dheeraj <karthikmy9@gmail.com>
2015-07-06 02:24:58 +0530

    unnecessary code removal

--- app.js
+++ app.js
@@ -8,9 +8,6 @@ var bodyParser = require('body-parser');
 var routes = require('./routes/index');
 var users = require('./routes/users');
 
-//Bullseye pages
-var login = require('./routes/login');
-
 var app = express();
 
 // view engine setup
